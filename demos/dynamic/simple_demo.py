b80dd2acdae6629eca2787d050bd3dfbb27676a1
Joakim Sundnes <sundnes@simula.no>
2026-06-30 13:33:27 +0200

    initial commit

---
+++ demos/dynamic/simple_demo.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#from scipy.integrate import solve_ivp
+from myogrid.dynamic import (
+    SarcArray2D,
+    contr_model,
+    plot_sarc_array, 
+    plot_results, 
+    get_results,
+    OverlapRice,
+    CaTransientFromFile
+)
+
+
+N_SERIES = 5
+N_FIBRILS = 3
+    
+overlap_func = OverlapRice(len_thin=1.4, len_thick=1.45)
+    
+try:
+    ca_func = CaTransientFromFile('../../data/ca_data/ca_trace.dat')
+except FileNotFoundError:
+    print("Warning: ca_trace.dat not found. Falling back to a dummy Ca transient.")
+    ca_func = lambda t: 0.001
+    
+array_model = SarcArray2D(
+    n_series=N_SERIES, 
+    n_fibrils=N_FIBRILS, 
+    model=contr_model, 
+    default_ca_func=ca_func,
+    overlap_func=overlap_func
+)
+    
+sample_sl = np.full((N_FIBRILS, N_SERIES), 1.85)
+sample_sl[1, 1:4] = [1.8, 1.8, 1.95]
+highlight_sarcs = [(1, 1), (1, 2), (1, 3)]
+    
+#plot the sarc array and save the color map for later:
+colors = plot_sarc_array('sarcomere_visual.pdf', sample_sl, plot_sarcs=highlight_sarcs)
+    
+init = array_model.init_state_values({
+    (1, 1): {'SL': 1.8}, 
+    (1, 2): {'SL': 1.8}, 
+    (1, 3): {'SL': 1.95}
+})
+    
+params = array_model.init_parameter_values({
+    'k_im': 0.05, 
+    'k_se': 100, 
+    'all': {'T_ref': 2.0, 'SLrest': 1.85},
+    (1, 1): {'SLset': 1.8},
+    (1, 2): {'SLset': 1.8},
+    (1, 3): {'SLset': 1.95}
+})
+    
+t, s = array_model.simulate(800, init, (params,), method='Radau')
+SLind = contr_model.state_indices("SL")
+t0, s0 = get_results(t, s, N_SERIES, N_FIBRILS, SLind, highlight_sarcs)    
+    
+plt.figure()
+for i in range(len(highlight_sarcs)):
+    plt.plot(t0, s0[:, i], color=colors[i], label=f'Sarc {highlight_sarcs[i]}')
+
+plt.xlabel('Time (ms)')
+plt.ylabel('Sarcomere Length (μm)')
+plt.legend()
+
+plt.show()
+  
---
+++ demos/dynamic/tow_3by5.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+#from scipy.integrate import solve_ivp
+from myogrid.dynamic import (
+    SarcArray2D,
+    contr_model,
+    plot_sarc_array, 
+    get_results,
+    OverlapRice,
+    CaTransientFromFile
+)
+
+
+def main():
+    figure_dir = 'tow_plots'
+    if not os.path.isdir(figure_dir):
+        os.makedirs(figure_dir)
+
+    no_sarc_series = 5
+    no_myofib = 3
+
+    
+    # These four lines are for plotting the sarcomere array and saving the color map:
+    SLarr = np.array([[2.0] * no_sarc_series, [2.0] * no_sarc_series, [2.0] * no_sarc_series])
+    sarcs_to_plot = [(1, 1), (1, 2), (1, 3)]
+    filename = os.path.join(figure_dir, 'tow_array.pdf')
+    colors = plot_sarc_array(filename, SLarr, sarcs_to_plot, True)
+
+
+    overlap_func = OverlapRice(len_thin=1.4, len_thick=1.45)
+    hf_shortening_ca = CaTransientFromFile('../../data/ca_data/hf_shortening.dat', scale=1.0)
+    hf_tow_ca = CaTransientFromFile('../../data/ca_data/hf_tow.dat', scale=1.0)
+
+    sarc3by5 = SarcArray2D(
+        no_sarc_series, 
+        no_myofib, 
+        contr_model, 
+        default_ca_func=hf_shortening_ca, 
+        overlap_func=overlap_func,
+        ca_func_variations={(1, 2): hf_tow_ca}
+    )
+    
+    SLind = contr_model.state_indices("SL")
+    t_stop = 1000
+    SL = 2.0
+
+    init = sarc3by5.init_state_values({
+        'all': {'SL': 2.0}, 
+        (1, 2): {'SL': SL}
+    })
+    
+    params = sarc3by5.init_parameter_values({
+        'k_im': 0.1, 
+        'k_se': 0.25,
+        'all': {'Cp': 0.002, 'b_ff': 10, 'xbmodsp': 1.0, 'visc': 3.0, 'T_ref': 15.0, 'SLrest': 2.0, 'SLset': 2.0},
+        (1, 1): {'Cp': 0.002, 'b_ff': 10, 'xbmodsp': 1.0, 'visc': 3.0, 'T_ref': 15.0, 'SLrest': 2.0, 'SLset': SL}
+    })
+
+    
+    p = (params,)
+    
+    #solve ODEs and extract results from selected plots:
+    t, s = sarc3by5.simulate(t_stop, init, p, method='Radau')
+    t0, s0 = get_results(t, s, no_sarc_series, no_myofib, SLind, sarcs_to_plot)
+    
+
+    plt.figure()
+    for i in range(len(sarcs_to_plot)):
+        plt.plot(t0, s0[:, i], color=colors[i], label=f'Sarc {sarcs_to_plot[i]}')
+
+
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Sarcomere Length (μm)')
+    plt.legend()
+    
+    filename = os.path.join(figure_dir, 'tow_curves.pdf')
+    plt.savefig(filename)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
